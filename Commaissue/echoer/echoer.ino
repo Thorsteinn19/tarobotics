51de7384f53fa2989582d088d5b6fa2287a71308
Thorsteinn19 <thorsteinn19@ru.is>
2022-04-12 16:23:00 +0000

    newhex

--- Commaissue/echoer/echoer.ino
+++ Commaissue/echoer/echoer.ino
@@ -1,4 +1,4 @@
-int incomingByte = 0;    // for incoming serial data
+ // for incoming serial data
 
 void setup() {
     Serial.begin(9600);    // opens serial port, sets data rate to 9600 bps
@@ -9,10 +9,10 @@ void loop() {
   if (Serial.available() > 0) {
   
     // read the incoming byte:
-    incomingByte = Serial.read();
+    String incomingByte = Serial.readString();
   
     // say what you got:
-    Serial.println((char)incomingByte);
+    Serial.println(incomingByte);
   }
   
 }
