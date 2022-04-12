753f4662693e35f54e92e54c2d0b7f6273dd82ce
Thorsteinn19 <thorsteinn19@ru.is>
2022-04-12 16:40:22 +0000

    Update echo.py

--- Commaissue/echo.py
+++ Commaissue/echo.py
@@ -1,8 +1,10 @@
 import serial
 
-serialport=serial.Serial(port="/dev/ttyS0",baudrate=9600, bytesize=8, timeout=10, stopbits=serial.STOPBITS_ONE)
+serialport=serial.Serial(port="/dev/ttyS0",baudrate=9600, timeout=1)
 
 
-serialport.write(b'hallo')
+serialport.write(bytes("a","utf-8"))
 while 1:
-    print(ser.readline().decode('ascii'))
+    if serialport.inWaiting():
+        inputstring=ser.readline().decode('utf-8')
+        print(inputstring)
